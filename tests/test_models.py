d4bd4ffd6e00a0edd1f8ff3b616123daa8f04a06
Milind Shakya <milind.shakya@dealertrack.com>
2015-11-04 20:59:49 -0200

    Initial Commit. Added files.

---
+++ tests/test_models.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+"""
+test_testpad
+------------
+
+Tests for `testpad` models module.
+"""
+
+from django.test import TestCase
+
+from testpad import models
+
+
+class TestTestpad(TestCase):
+
+    def setUp(self):
+        pass
+
+    def test_something(self):
+        pass
+
+    def tearDown(self):
+        pass
